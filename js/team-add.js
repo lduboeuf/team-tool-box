8c9adaa14f8704567e2ad73c73d955293b2b319f
lduboeuf <lduboeuf@ouvaton.org>
2016-12-05 12:33:52 +0100

    a little more style

--- js/team-add.js
+++ js/team-add.js
@@ -2,16 +2,22 @@
 
 app.page("team-add", function()
 {
-
+  var nameInput = document.querySelector('form input[name="member-name"]');
   var submitBtn = document.querySelector('form input[name="add-member"]');
+
+
   submitBtn.onclick = function(e){
     e.preventDefault();
-    var nameInput = document.querySelector('form input[name="member-name"]');
-    TeamRepository.save({ "name": nameInput.value});
-    //refresh view
-    //hack:..., not compatible old browser grrr.....
-    history.pushState(null, null, "#team-list");
-    app("team-list");
+    if (nameInput.value.length>0){
+      TeamRepository.save({ "name": nameInput.value});
+      //refresh view
+      //hack:..., not compatible old browser grrr.....
+      history.pushState(null, null, "#team-list");
+      app("team-list");
+    }else {
+      //handle error msg
+    }
+
 
   }
 
