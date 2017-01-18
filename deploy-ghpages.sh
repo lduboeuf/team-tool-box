d6b09c80b40378df0232b04d06ce17bebb61c4fa
lduboeuf <lduboeuf@ouvaton.org>
2017-01-18 09:24:17 +0100

    publish to gh-pages site

--- deploy-ghpages.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-git add dist && git commit -m "publish to gh-pages site"
-#git subtree push --prefix dist origin gh-pages
-git push origin `git subtree split --prefix dist master`:gh-pages --force
