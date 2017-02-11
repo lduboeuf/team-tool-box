36f2fe2213063bc80762bab332a2d4b41c6fcb03
lduboeuf <lionel.duboeuf@gmail.com>
2017-02-11 22:05:35 +0100

    publish to gh-pages site

---
+++ dist/dev/app.js
@@ -0,0 +1,65 @@
+//alert msg handler
+app.alert = function(type, message){
+  var className='.'+type;
+  var alertBox = document.querySelector(className);
+  var alertMsg = alertBox.querySelector('.alert-msg');
+  alertMsg.innerHTML = message;
+  alertBox.style.display = 'block';
+  window.addEventListener('click', app.cleanUI);
+}
+
+
+app.cleanUI = function(evt){
+  //toggle menu when a menu item is clicked
+  if (evt.target.parentNode.className!=='icon'){
+    document.getElementById("menu").className = "topnav";
+    window.removeEventListener('click', app.cleanUI);
+  }
+  //make sure every alert box is hidden
+  document.querySelector('.alert').style.display = 'none';
+
+}
+
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  //menu handler
+  var $menu = document.getElementById("menu");
+  var $menu_icon = $menu.querySelector('.icon > a');
+  //menu for mobile click handler
+  function toggleMenu() {
+      if ($menu.className === "topnav") {
+          $menu.className += " responsive";
+          //handle click outside menu to toggle it
+          window.addEventListener('click', app.cleanUI);
+      } else {
+          $menu.className = "topnav";
+      }
+  }
+
+
+  $menu_icon.onclick = toggleMenu;
+
+  //remotestorage
+  remoteStorage.access.claim('teams', 'rw');
+  remoteStorage.access.claim('archives', 'rw');
+  remoteStorage.access.claim('config', 'rw');
+
+  remoteStorage.on('ready', function() {
+    //more optimized than findAll ?
+    remoteStorage.teams.findAll().then(
+      function(teams){
+
+        if (Object.keys(teams).length === 0){
+          app.alert('alert-info','<strong>hello ;-)</strong>. Welcome to <strong>Team Tool Box!</strong>. Maybe your first time here, please have a look to the <a href="#about">About</a> section. For those who may not retrieve your datas, be sure to be synced with remoteStorage or see <a href="#settings">settings</a> to upgrade');
+        }
+      }
+    );
+  });
+
+
+  remoteStorage.displayWidget();
+
+
+  //simulate a hash change at startup
+  window.dispatchEvent(new CustomEvent('hashchange'));
+});
---
+++ dist/dev/remotestorage/remotestorage-ttb-teams.js
@@ -0,0 +1,160 @@
+RemoteStorage.defineModule("teams", function (privateClient, publicClient) {
+
+  privateClient.cache('', 'ALL');
+  privateClient.declareType('team', {
+    "type": "object",
+    "properties": {
+      "id": {
+        "type": "string"
+      },
+      "name": {
+      "type": "string"
+      },
+      "members": {
+        "type": "array",
+        "default": [],
+        "items":{
+          "type": "object",
+          "properties": {
+            "id": {
+              "type": "string"
+            },
+            "name": {
+            "type": "string"
+            }
+          }
+        }
+      }
+    },
+    "required": [ "id", "name" ]
+  });
+
+  var upgrade = function(client){
+    return client.getAll("team/").then(function(teams){
+
+      var toStore = [];
+      var toDelete = [];
+      for (var teamId in teams) {
+        toStore.push(client.storeObject('',teamId,teams[teamId]));
+        toDelete.push(client.remove('team/' + teamId));
+      }
+      toDelete.push(client.remove('team/'));
+      if (toStore.length==0) return false;
+      return Promise.all(toStore).then(function() {
+          console.log("items moved from teams/team/ to teams/");
+          return Promise.all(toStore).then(function() {
+            console.log('items removed at teams/team/*');
+            return true;
+          }).catch(function(e) {
+            console.log('oups...' + e);
+            return false;
+          })
+      })
+      .catch(function(e) {
+          console.log('oups...' + e);
+          return false;
+      });
+
+    });
+  }
+
+  var teams = {
+
+        upgrade: function() {
+          return upgrade(privateClient);
+        },
+
+        store: function(team) {
+          if (!team.id){
+            team.id = generateUID();
+            team.members = [];
+          }
+          return privateClient.storeObject("team", team.id, team).
+            then(function() {
+              return team;
+            });
+        },
+        addMember: function(teamId, member){
+          return remoteStorage.teams.find(teamId).then(
+            function(team){
+              member.id = generateUID();
+              team.members.push(member);
+              return remoteStorage.teams.store(team);
+            });
+        },
+        findMember: function(memberId){
+          return privateClient.getAll('').then(function(teams){
+            for (var property in teams) {
+              var member = findById(teams[property].members, memberId);
+              if (member){
+                return member;
+              }
+            }
+            return null;
+          })
+        },
+        updateMember: function(member){
+          return privateClient.getAll('').then(function(teams){
+            for (var property in teams) {
+              var members = teams[property].members;
+              for (var i=0; i < members.length; i++){
+                  if (members[i].id === member.id){
+                    members[i] = member;
+                    return remoteStorage.teams.store(teams[property]);
+                  }
+
+              }
+            }
+            return null;
+          })
+
+        },
+        removeMember: function(memberId){
+          return privateClient.getAll('').then(function(teams){
+            for (var property in teams) {
+              var members = teams[property].members;
+              for (var i=0; i < members.length; i++){
+                  if (members[i].id === memberId){
+                      members.splice(i, 1);
+                      return remoteStorage.teams.store(teams[property]);
+                  }
+              }
+            }
+            return false;
+          })
+        },
+        remove: function(teamId){
+          return privateClient.remove(teamId);
+        },
+        find: function(id) {
+          return privateClient.getObject(id);
+        },
+        findAll: function(){
+          return privateClient.getAll("");
+        }
+  };
+
+  var ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var ID_LENGTH = 8;
+
+  var generateUID = function() {
+    var rtn = '';
+    for (var i = 0; i < ID_LENGTH; i++) {
+      rtn += ALPHABET.charAt(Math.floor(Math.random() * ALPHABET.length));
+    }
+    return rtn;
+  }
+
+  var findById = function(collection, id){
+    for (var i = 0; i < collection.length; i++) {
+      if (collection[i].id === id) {
+        return collection[i];
+      }
+    }
+    return null;
+  }
+
+    // Return and export public functions
+   return { exports: teams };
+
+});
