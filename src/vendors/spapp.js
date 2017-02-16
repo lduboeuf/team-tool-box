d0f2614660a55ac0ef0cf7051df4f9ca5a756e55
lduboeuf <lduboeuf@ouvaton.org>
2017-02-16 11:53:26 +0100

    remove match function

--- src/vendors/spapp.js
+++ src/vendors/spapp.js
@@ -26,12 +26,6 @@
   var currentPageName = null;
   var oldPageName = null;
   var $currentPage = null;
-  //equivalent of "is" jquery function
-  var matches = function(el, selector) {
-    return (el.matches || el.matchesSelector || el.msMatchesSelector || el.mozMatchesSelector || el.webkitMatchesSelector || el.oMatchesSelector).call(el, selector);
-  };
-
-
 
   function show(pageName,param) {
     var $page = document.querySelector("section#" + pageName);
@@ -45,21 +39,20 @@
       var that = $page.length > 0 ? $page[0] : null;
       var r = ph.call(that , param);
       if( typeof r == "function" ) { // it returns the function that's used for view rendering
-        if (!matches($page, '[no-ctl-cache]'))
+        if(!$page.hasAttribute('[no-ctl-cache]')) //for non cached section
             pageHandlers[pageName] = r;
         r.call(that, param); // call that rendering function
       }
     }
     if(currentPageName) { // "close" current page view
       //
-      if (modal!==true){ //when modal keep currentPage in background
+      if (modal!==true){ //when modal, keep currentPage in background
         document.body.classList.remove(currentPageName); // remove old page class
+
       }
       if($currentPage) {
-
         document.dispatchEvent(new CustomEvent('page.hidden',{'currentPage' : currentPageName }));
-        if($currentPage.getAttribute('src') && matches($currentPage, '[no-ctl-cache]'))
-            $currentPage.innerHTML = null;
+        if($currentPage.hasAttribute('[no-ctl-cache]')) $currentPage.innerHTML = null;
       }
     }
     oldPageName = currentPageName;
@@ -71,7 +64,7 @@
 
       document.dispatchEvent(new CustomEvent('page.shown', {'detail' : {'currentPage' : currentPageName, 'title': $page.getAttribute('title')}}));
       //update url location if not
-      if ($page.getAttribute('default')!=""){
+      if ($page.hasAttribute('default')){
         var url = '#' + currentPageName;
         if (param && typeof(param)!=='object') //don't display object in url
           url += ':' + param;
@@ -92,9 +85,6 @@
     var src = $page.getAttribute("src");
     if( src && !$page.hasChildNodes()) { // it has src and is empty
       app.get(src, $page, pageName, param);
-      // app.get(src, $page, pageName)
-      //     .done(function(html){$page.html(html); show(pageName,param); })
-      //     .fail(function(){ console.warn("failed to get %s page!",pageName);});
     } else
       show(pageName,param);
   }
@@ -109,7 +99,6 @@
 
   // Function to get page's html, shall return jQuery's promise. Can be overriden.
   app.get = function(src,$page,pageName, param) {
-    //return $.get(src, "html");
     var request = new XMLHttpRequest();
     request.open('GET', src, true);
 
@@ -144,16 +133,8 @@
   }
 
   window.addEventListener('hashchange', onhashchange);
-  //$(window).on("hashchange", onhashchange );
 
   window.app = app;
 
-  // Call onhashchange manually for the initialization.
-  // setTimeout is used to postpone initialization so application can configure window.app.get = function(src,$page,pageName); for example
-  //don't work everytime
-  // window.setTimeout( function() {
-  //   window.dispatchEvent(new CustomEvent('hashchange'));
-  //   //$(onhashchange);
-  // });
 
 })(this);
