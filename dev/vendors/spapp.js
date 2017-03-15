f2652feead44e86366cc269ec691f00155042a3b
lduboeuf <lduboeuf@ouvaton.org>
2017-03-15 10:35:44 +0100

    code into webcomponents

--- dev/vendors/spapp.js
+++ dev/vendors/spapp.js
@@ -26,57 +26,45 @@
   var currentPageName = null;
   var oldPageName = null;
   var $currentPage = null;
-  //equivalent of "is" jquery function
-  var matches = function(el, selector) {
-    return (el.matches || el.matchesSelector || el.msMatchesSelector || el.mozMatchesSelector || el.webkitMatchesSelector || el.oMatchesSelector).call(el, selector);
-  };
 
-
-
-  function show(pageName,param, modal) {
+  function show(pageName,param) {
     var $page = document.querySelector("section#" + pageName);
     if( $page.length == 0 ) {
       console.warn("section with id=%s not found!",pageName);
       return;
     }
+    var modal = $page.classList.contains('modal');
     var ph = pageHandlers[pageName];
     if( ph ) {
       var that = $page.length > 0 ? $page[0] : null;
       var r = ph.call(that , param);
       if( typeof r == "function" ) { // it returns the function that's used for view rendering
-        if (!matches($page, '[no-ctl-cache]'))
-        //if(!$page.is("[no-ctl-cache]"))
+        if(!$page.hasAttribute('[no-ctl-cache]')) //for non cached section
             pageHandlers[pageName] = r;
         r.call(that, param); // call that rendering function
       }
     }
     if(currentPageName) { // "close" current page view
       //
-      if (modal!==true){
+      if (modal!==true){ //when modal, keep currentPage in background
         document.body.classList.remove(currentPageName); // remove old page class
-        $currentPage.classList.remove('modal');
+
       }
       if($currentPage) {
-
         document.dispatchEvent(new CustomEvent('page.hidden',{'currentPage' : currentPageName }));
-        //$currentPage.trigger("page.hidden",currentPageName);
-        if($currentPage.getAttribute('src') && matches($currentPage, '[no-ctl-cache]'))
-            $currentPage.innerHTML = null;
-            //$currentPage.empty();
+        if($currentPage.hasAttribute('[no-ctl-cache]')) $currentPage.innerHTML = null;
       }
     }
     oldPageName = currentPageName;
     document.body.classList.add(currentPageName = pageName); // set new page class
-    if (modal===true){
-      $page.classList.add('modal');
-    }
+
 
 
     if($currentPage = $page){
 
-      document.dispatchEvent(new CustomEvent('page.shown', {'currentPage' : currentPageName}));
+      document.dispatchEvent(new CustomEvent('page.shown', {'detail' : {'currentPage' : currentPageName, 'title': $page.getAttribute('title')}}));
       //update url location if not
-      if ($page.getAttribute('default')!=""){
+      if ($page.hasAttribute('default')){
         var url = '#' + currentPageName;
         if (param && typeof(param)!=='object') //don't display object in url
           url += ':' + param;
@@ -88,7 +76,7 @@
     }
   }
 
-  function app(pageName,param, modal) {
+  function app(pageName,param) {
 
     var $page = document.body.querySelector("section#" + pageName);
     if(!$page){
@@ -96,12 +84,9 @@
     }
     var src = $page.getAttribute("src");
     if( src && !$page.hasChildNodes()) { // it has src and is empty
-      app.get(src, $page, pageName, param,modal);
-      // app.get(src, $page, pageName)
-      //     .done(function(html){$page.html(html); show(pageName,param); })
-      //     .fail(function(){ console.warn("failed to get %s page!",pageName);});
+      app.get(src, $page, pageName, param);
     } else
-      show(pageName,param,modal);
+      show(pageName,param);
   }
 
   app.back = function(params){
@@ -113,6 +98,7 @@
     };
 
   // Function to get page's html, shall return jQuery's promise. Can be overriden.
+  // Function to get page's html, shall return jQuery's promise. Can be overriden.
   app.get = function(src,$page,pageName, param, modal) {
     //return $.get(src, "html");
     var request = new XMLHttpRequest();
@@ -122,6 +108,10 @@
       if (request.status >= 200 && request.status < 400) {
         // Success!
         $page.innerHTML = request.responseText;
+        $script = $page.querySelector('script');
+        if ($script){
+          eval($script.text);
+        }
         show(pageName,param, modal);
       } else {
         // We reached our target server, but it returned an error
@@ -149,16 +139,8 @@
   }
 
   window.addEventListener('hashchange', onhashchange);
-  //$(window).on("hashchange", onhashchange );
 
   window.app = app;
 
-  // Call onhashchange manually for the initialization.
-  // setTimeout is used to postpone initialization so application can configure window.app.get = function(src,$page,pageName); for example
-  //don't work everytime
-  // window.setTimeout( function() {
-  //   window.dispatchEvent(new CustomEvent('hashchange'));
-  //   //$(onhashchange);
-  // });
 
 })(this);
